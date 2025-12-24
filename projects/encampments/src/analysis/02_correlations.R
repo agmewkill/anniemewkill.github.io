5ad0b7c0c0ea8b754e84072edee6572cf0397f48
agmewkill <amewkill9780@sdsu.edu>
2025-12-24 13:53:54 -0800

    Add correlation analysis for 2020 data

--- projects/encampments/src/analysis/02_correlations.R
+++ projects/encampments/src/analysis/02_correlations.R
@@ -1,1 +1,22 @@
+library(dplyr)
+library(psych)
+
+df <- read_csv("data/outputs/tract_acs_pitc_2020.csv")
+
+vars <- df %>%
+  select(
+    pit_total,
+    pct_below_poverty,
+    pct_unemployed,
+    pct_hispanic,
+    median_income
+  ) %>%
+  na.omit()
+
+corr <- corr.test(vars, method = "pearson")
+
+write_csv(
+  as.data.frame(corr$r),
+  "data/outputs/correlations_2020.csv"
+)
 
